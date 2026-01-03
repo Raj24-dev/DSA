e266d0c5e419965acb1f9aca42e3980728063882
Raj24-dev <raj24wc@gmail.com>
2026-01-03 13:44:05 +0530

    recurssion

--- reverse_a_number.cpp
+++ reverse_a_number.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int reverseNumber(int n) {
   int rem = 0;
         int rev = 0;
-       while(n>0){
+        while(n>0){
         rem = n%10;
         rev = rev*10+rem;
         n=n/10;
